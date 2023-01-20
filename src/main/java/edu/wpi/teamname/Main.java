0d2bb5df45b0225eee4c98402177eec91803568b
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-01-20 14:43:16 -0500

    Initial commit

---
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -0,0 +1,10 @@
+package edu.wpi.teamname;
+
+public class Main {
+
+  public static void main(String[] args) {
+    App.launch(App.class, args);
+    System.out.println("Cierra");
+    System.out.println("Jolie")
+  }
+}
