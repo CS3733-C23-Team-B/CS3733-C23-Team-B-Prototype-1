ee5f078d5080e918bdd72d4d4c3e8e9d250ae89d
swlendrum <slendrum465@gmail.com>
2023-01-21 17:29:11 -0500

    Create pathfinding branch

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,8 +1,16 @@
 package edu.wpi.teamname;
 
+import java.util.PriorityQueue;
+
 public class Main {
 
+  private int x, y;
+
   public static void main(String[] args) {
+
     App.launch(App.class, args);
+
+    PriorityQueue queue = new PriorityQueue();
+
   }
 }
