e7a784094f8c802c00e7b001c58783ad499bd4c5
lukegrady1 <lukegrady9@outlook.com>
2023-01-22 14:51:33 -0500

    Updates to Feature2

--- src/main/java/edu/wpi/teamname/Controllers/feature2Controller.java
+++ src/main/java/edu/wpi/teamname/Controllers/feature2Controller.java
@@ -1,35 +1,32 @@
 package edu.wpi.teamname.Controllers;
 
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
-import java.io.IOException;
-
 public class feature2Controller {
-    @FXML private MFXTextField firstName;
-    @FXML private MFXTextField lastName;
-    @FXML private MFXTextField employeeID;
-    @FXML private MFXTextField email;
-    @FXML private MFXTextField cleanUpLocation;
-    @FXML private ChoiceBox urgency;
-    @FXML private ChoiceBox typeOfCleanUp;
-    @FXML private ChoiceBox areaOfCleanUp;
-    @FXML private Button clearButton;
-    @FXML private Button submitButton;
+  @FXML private MFXTextField firstName;
+  @FXML private MFXTextField lastName;
+  @FXML private MFXTextField employeeID;
+  @FXML private MFXTextField email;
+  @FXML private ChoiceBox cleanUpLocation;
+  @FXML private ChoiceBox urgency;
+  @FXML private ChoiceBox typeOfCleanUp;
+  @FXML private ChoiceBox areaOfCleanUp;
+  @FXML private Button clearButton;
+  @FXML private Button submitButton;
 
-    public void initialize() {
-        // intialization goes here
-    }
+  public void initialize() {
+    // intialization goes here
+  }
 
-    public void clearButtonClicked() throws IOException {
-        // Stuff to handle the clearing of fields goes here
-    }
+  public void clearButtonClicked() throws IOException {
+    // Stuff to handle the clearing of fields goes here
+  };
 
-    public void submitButtonClicked() throws IOException {
-        // Stuff to handle submitting
-    }
+  public void submitButtonClicked() throws IOException {
+    // Stuff to handle submitting
+  }
 }
