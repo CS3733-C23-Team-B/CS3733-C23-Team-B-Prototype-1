b8b0afcce35e04c54a09eab845469fbe1d0aeedb
JackLafond <jacklafo12@gmail.com>
2023-01-22 17:02:30 -0500

    Update Bdb and Node
    new Node class, update to db file, attempt 2 at driver register, and skeleton for necessary methods in Bdb

---
+++ src/main/java/edu/wpi/teamname/database/Bdb.java
@@ -0,0 +1,46 @@
+package edu.wpi.teamname.database;
+
+import java.sql.*;
+
+public class Bdb {
+
+  public static void main(String[] args) {
+
+    try {
+      Class.forName("org.postgresql.Driver");
+
+    } catch (ClassNotFoundException e) {
+      System.out.println("Driver Not Found, Add Classpath To Module");
+      e.printStackTrace();
+      return;
+    }
+
+    System.out.println("Driver Registered");
+    Connection connect = null;
+
+    try {
+      connect =
+          DriverManager.getConnection(
+              "jdbc:postgresql://database.cs.wpi.edu:5432/teambdb", "teamb", "teamb20");
+
+    } catch (Exception e) {
+      System.out.println("Connection Failed");
+      e.printStackTrace();
+      return;
+    }
+
+    System.out.println("Connection Successful");
+  }
+
+  public void getNode(){};
+
+  public void updateNode(){};
+
+  public void deleteNode(){};
+
+  public void getEdge(){};
+
+  public void updateEdge(){};
+
+  public void deleteEdge(){};
+}
---
+++ src/main/java/edu/wpi/teamname/database/Node.java
@@ -0,0 +1,30 @@
+package edu.wpi.teamname.database;
+
+public class Node {
+
+    private String nodeID;
+    private int xcoord;
+    private int ycoord;
+    private String floor;
+    private String building;
+    private String nodeType;
+    private String longName;
+    private String shortName;
+
+    public Node(String nodeID, int xcoord, int ycoord,
+                String floor, String building, String nodeType,
+                String longName, String shortName) {
+
+        this.nodeID = nodeID;
+        this.xcoord = xcoord;
+        this.ycoord = ycoord;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+    }
+
+
+
+}
