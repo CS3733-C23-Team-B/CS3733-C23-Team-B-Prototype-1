ae2d1e369c539f64cafdd2ba2a632fbd759d9e4c
Jwalts23 <81712807+Jwalts23@users.noreply.github.com>
2023-01-22 11:17:17 -0500

    added buttons and more controller skeleton

--- src/main/java/edu/wpi/teamname/Controllers/feature2Controller.java
+++ src/main/java/edu/wpi/teamname/Controllers/feature2Controller.java
@@ -2,7 +2,12 @@ package edu.wpi.teamname.Controllers;
 
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+
+import java.io.IOException;
 
 public class feature2Controller {
     @FXML private MFXTextField firstName;
@@ -13,8 +18,18 @@ public class feature2Controller {
     @FXML private ChoiceBox urgency;
     @FXML private ChoiceBox typeOfCleanUp;
     @FXML private ChoiceBox areaOfCleanUp;
+    @FXML private Button clearButton;
+    @FXML private Button submitButton;
+
     public void initialize() {
         // intialization goes here
     }
-    
+
+    public void clearButtonClicked() throws IOException {
+        // Stuff to handle the clearing of fields goes here
+    }
+
+    public void submitButtonClicked() throws IOException {
+        // Stuff to handle submitting
+    }
 }
