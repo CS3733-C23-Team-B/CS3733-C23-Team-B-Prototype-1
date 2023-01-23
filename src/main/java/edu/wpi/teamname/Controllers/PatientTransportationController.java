cf8d1e41e2e4e0f610c1dc554315fb42b5713568
Cierra <cierra362@gmail.com>
2023-01-22 22:44:47 -0500

    set up Controller file

---
+++ src/main/java/edu/wpi/teamname/Controllers/PatientTransportationController.java
@@ -0,0 +1,48 @@
+package edu.wpi.teamname.Controllers;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class PatientTransportationController {
+    @FXML private TextField firstName;
+    @FXML private TextField lastName;
+    @FXML private TextField employeeID;
+    @FXML private TextField email;
+    @FXML private ChoiceBox equipmentNeeded;
+    @FXML private ChoiceBox urgency;
+    @FXML private TextField patientLocation;
+    @FXML private TextField patientDestination;
+
+    @FXML private TextField notes;
+
+    @FXML private Button cancelButton;
+
+    @FXML private Button helpButton;
+
+    @FXML private Button clearButton;
+    @FXML private Button submitButton;
+
+    public void initialize() {
+        // intialization goes here
+    }
+
+    public void cancelButtonClicked() throws IOException {
+        // Stuff to handle moving back to home screen
+    }
+
+    public  void helpButtonClicked() throws IOException {
+        // Stuff to handle help screen
+    }
+
+    public void clearButtonClicked() throws IOException {
+        // Stuff to handle the clearing of fields goes here
+    };
+
+    public void submitButtonClicked() throws IOException {
+        // Stuff to handle submitting
+    }
+}
