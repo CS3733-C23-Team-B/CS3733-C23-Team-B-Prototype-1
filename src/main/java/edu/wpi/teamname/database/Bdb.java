68e27c69d0994e1360df654062294582e61e4d71
JackLafond <jacklafo12@gmail.com>
2023-01-23 14:17:55 -0500

    Fix to connection and Node

--- src/main/java/edu/wpi/teamname/database/Bdb.java
+++ src/main/java/edu/wpi/teamname/database/Bdb.java
@@ -7,43 +7,77 @@ public class Bdb {
   private Connection c;
   private static Bdb db;
 
-  public static void main(String[] args) {
-
+  static {
     try {
-      Class.forName("org.postgresql.Driver");
-
-    } catch (ClassNotFoundException e) {
-      System.out.println("Driver Not Found, Add Classpath To Module");
-      e.printStackTrace();
-      return;
+      db = new Bdb();
+      db.init();
+    } catch (ClassNotFoundException | SQLException e) {
+      throw new RuntimeException(e);
     }
-
-    System.out.println("Driver Registered");
-    Connection connect = null;
-
-    try {
-      connect =
-          DriverManager.getConnection(
-              "jdbc:postgresql://database.cs.wpi.edu:5432/teambdb", "teamb", "teamb20");
-
-    } catch (Exception e) {
-      System.out.println("Connection Failed");
-      e.printStackTrace();
-      return;
-    }
-
-    System.out.println("Connection Successful");
   }
 
-  public void getNode() {};
+  public Bdb() throws ClassNotFoundException, SQLException {
+    Class.forName("org.postgresql.Driver");
+    c = DriverManager.getConnection(
+            "jdbc:postgresql://database.cs.wpi.edu:5432/teambdb",
+            "teamb",
+            "teamb20");
+  }
 
-  public void updateNode() {};
+  public void init() throws SQLException {
+    if (!tableExists(Node.getTableName())) {
+      Node.initTable();
+    }
+    if (!tableExists(Edge.getTableName())) {
+      Edge.initTable();
+    }
+  }
 
-  public void deleteNode() {};
+  public static PreparedStatement prepareStatement(String s) throws SQLException {
+    return getInstance().c.prepareStatement(s);
+  }
 
-  public void getEdge() {};
+  public static PreparedStatement prepareKeyGeneratingStatement(String s) throws SQLException {
+    return getInstance().c.prepareStatement(s, Statement.RETURN_GENERATED_KEYS);
+  }
 
-  public void updateEdge() {};
+  public static ResultSet processQuery(String s) throws SQLException {
+    Statement stmt = getInstance().c.createStatement();
+    ResultSet rs = stmt.executeQuery(s);
+    return rs;
+  }
 
-  public void deleteEdge() {};
+  public static int processUpdate(String s) throws SQLException {
+    Statement stmt = getInstance().c.createStatement();
+    int numUpdated = stmt.executeUpdate(s);
+    return numUpdated;
+  }
+
+  public static void closeConnection() throws SQLException {
+    getInstance().c.close();
+    db = null;
+  }
+
+  public static boolean tableExists(String tableName) throws SQLException {
+    String query =
+            "SELECT EXISTS (\n"
+                    + "SELECT FROM\n"
+                    + "   pg_tables\n"
+                    + "WHERE\n"
+                    + "   schemaname = 'public' AND\n"
+                    + "   tablename  = ?\n"
+                    + ");\n";
+
+    PreparedStatement stmt = getInstance().c.prepareStatement(query);
+    stmt.setString(1, tableName);
+    ResultSet rs = stmt.executeQuery();
+    rs.next();
+
+    return rs.getBoolean(1);
+  }
+
+  public static Bdb getInstance() {
+    return db;
+  }
 }
+
--- src/main/java/edu/wpi/teamname/database/Node.java
+++ src/main/java/edu/wpi/teamname/database/Node.java
@@ -1,7 +1,13 @@
 package edu.wpi.teamname.database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List; // import java.util.stream.Stream;
 public class Node {
 
+  public static final String tableName = "Node";
   private String nodeID;
   private int xcoord;
   private int ycoord;
@@ -12,14 +18,23 @@ public class Node {
   private String shortName;
 
   public Node(
-      String nodeID,
-      int xcoord,
-      int ycoord,
-      String floor,
-      String building,
-      String nodeType,
-      String longName,
-      String shortName) {
+      int xcoord, int ycoord, String floor, String building,
+      String nodeType, String longName, String shortName) {
+
+    this.nodeID = null;
+    this.xcoord = xcoord;
+    this.ycoord = ycoord;
+    this.floor = floor;
+    this.building = building;
+    this.nodeType = nodeType;
+    this.longName = longName;
+    this.shortName = shortName;
+  }
+
+  public Node(
+          String nodeID,
+          int xcoord, int ycoord, String floor, String building,
+          String nodeType, String longName, String shortName) {
 
     this.nodeID = nodeID;
     this.xcoord = xcoord;
@@ -30,4 +45,84 @@ public class Node {
     this.longName = longName;
     this.shortName = shortName;
   }
+
+  public static void initTable() throws SQLException {
+    String sql =
+            String.join(
+                    " ",
+                    "CREATE TABLE Node",
+                    "(nodeID char(10),",
+                    "xcoord integer,",
+                    "ycoord integer,",
+                    "floor varchar(4),",
+                    "building varchar(15),",
+                    "nodeType char(4),",
+                    "longName varchar(70)",
+                    "shortName varchar(40));");
+    Bdb.processUpdate(sql);
+  }
+
+  public static List<Node> getAll() throws SQLException {
+    ArrayList<Node> things = new ArrayList<>();
+    String sql = "SELECT * FROM thing;";
+    ResultSet rs = Bdb.processQuery(sql);
+    while (rs.next()) {
+      things.add(new Node(rs.getString("nodeID"), rs.getInt("xcoord"), rs.getInt("ycoord"),
+              rs.getString("floor"), rs.getString("building"), rs.getString("nodeType"),
+              rs.getString("longName"), rs.getString("shortName")));
+    }
+    return things;
+  }
+
+  public void insert() throws SQLException {
+    String sql = "INSERT INTO Node (nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName) " +
+            "VALUES (?,?,?,?,?,?,?,?);";
+    PreparedStatement ps = Bdb.prepareKeyGeneratingStatement(sql);
+    ps.setString(1, nodeID);
+    ps.setInt(2, xcoord);
+    ps.setInt(3, ycoord);
+    ps.setString(4, floor);
+    ps.setString(5, building);
+    ps.setString(6, nodeType);
+    ps.setString(7, longName);
+    ps.setString(8, shortName);
+
+    /// not sure how we will deal with generating new nodeID yet but left at string for now
+    /// so ignore duplicate in update() for now
+
+    ps.executeUpdate();
+  }
+
+  public void update() throws SQLException {
+    String sql = "UPDATE Node SET NodeID = ?, xcoord = ?, ycoord = ?, floor = ?, " +
+            "building = ?, nodeType = ?, longName = ?, shortName = ?;";
+    PreparedStatement ps = Bdb.prepareStatement(sql);
+    ps.setString(1, nodeID);
+    ps.setInt(2, xcoord);
+    ps.setInt(3, ycoord);
+    ps.setString(4, floor);
+    ps.setString(5, building);
+    ps.setString(6, nodeType);
+    ps.setString(7, longName);
+    ps.setString(8, shortName);
+    ps.executeUpdate();
+  }
+
+  public void delete() throws SQLException {
+    String sql = "DELETE FROM thing WHERE nodeID = ?";
+    PreparedStatement ps = Bdb.prepareStatement(sql);
+    ps.setString(1, nodeID);
+    ps.executeUpdate();
+  }
+
+  public static String getTableName() {
+    return tableName;
+  }
+
+  public String getID() {
+    return nodeID;
+  }
+}
+
+
 }
