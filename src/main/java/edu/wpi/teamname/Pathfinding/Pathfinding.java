33f572f1cdc439516dc011792bcd8994bd5cf25d
swlendrum <slendrum465@gmail.com>
2023-01-23 14:55:21 -0500

    Implement depth/breadth-first algorithm
    This file contains the ability to easily switch between breadth-first and depth-first node traversal, with temporary example edges in case

--- src/main/java/edu/wpi/teamname/Pathfinding/Pathfinding.java
+++ src/main/java/edu/wpi/teamname/Pathfinding/Pathfinding.java
@@ -1,18 +1,88 @@
 package edu.wpi.teamname.Pathfinding;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Pathfinding {
 
-    String[][] edges;
-    public static String generatePath(String currLocation, String destination)
-    {
-        return "";
+  private static String[][] edges = {
+    {"a", "f"},
+    {"a", "b"},
+    {"b", "e"},
+    {"e", "c"},
+    {"f", "d"},
+    {"c", "g"},
+    {"f", "g"},
+    {"d", "h"},
+    {"h", "b"}
+  };
+
+  public static void main(String[] args) {
+    System.out.println(generatePath("a", "g"));
+  }
+
+  public static String generatePath(String currLocation, String destination) {
+    return getShortestPath(currLocation, destination, true);
+  }
+
+  private static ArrayList<String> getDirectPaths(String node) {
+    ArrayList<String> retList = new ArrayList<String>();
+    for (String[] edge : edges) {
+      if (edge[0].equals(node)) retList.add(edge[1]);
+      else if (edge[1].equals(node)) retList.add(edge[0]);
+    }
+    return retList;
+  }
+
+  private static String pathToString(ArrayList<String> path) {
+    String retStr = "";
+
+    for (String a : path) retStr += a + " -> ";
+
+    retStr = retStr.substring(0, retStr.length() - 4);
+    return retStr;
+  }
+
+  private static String getShortestPath(String start, String end, boolean depth) {
+    boolean done = false;
+    HashMap<String, String> cameFrom = new HashMap<String, String>();
+
+    ArrayList<String> toExpand = new ArrayList<String>();
+    toExpand.add(start);
+
+    while (toExpand.size() > 0) {
+
+      // Breadth-first:
+      String current = toExpand.remove(0);
+      // Depth-first:
+      //      String current = toExpand.remove(toExpand.size() - 1);
+
+      ArrayList<String> directPaths = getDirectPaths(current);
+      for (String path : directPaths) {
+        if (path.equals(end)) {
+          cameFrom.put(path, current);
+          done = true;
+          break;
+        }
+        if (!cameFrom.containsKey(path)) {
+          cameFrom.put(path, current);
+          toExpand.add(path);
+        }
+      }
+
+      if (done) break;
     }
 
-//    private ArrayList<String> getDirectPaths(String node)
-//    {
-//        retru
-//    }
+    if (!done) return "PATH NOT FOUND";
+
+    ArrayList<String> path = new ArrayList<>();
+    path.add(start);
+
+    String current = end;
+    while (!current.equals(start)) {
+      path.add(1, current);
+      current = cameFrom.get(current);
+    }
+
+    return pathToString(path);
+  }
 }
