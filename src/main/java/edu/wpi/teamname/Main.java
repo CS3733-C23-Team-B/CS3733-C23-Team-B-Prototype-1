1b21b1b17c6df542bb79bb463fe65c5896c9546f
swlendrum <slendrum465@gmail.com>
2023-01-22 20:23:13 -0500

    Initial Changes

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,16 +1,6 @@
 package edu.wpi.teamname;
 
-import java.util.PriorityQueue;
-
 public class Main {
 
-  private int x, y;
-
-  public static void main(String[] args) {
-
-    App.launch(App.class, args);
-
-    PriorityQueue queue = new PriorityQueue();
-
-  }
+  public static void main(String[] args) { App.launch(App.class, args); }
 }
---
+++ src/main/java/edu/wpi/teamname/Pathfinding/Pathfinding.java
@@ -0,0 +1,18 @@
+package edu.wpi.teamname.Pathfinding;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Pathfinding {
+
+    String[][] edges;
+    public static String generatePath(String currLocation, String destination)
+    {
+        return "";
+    }
+
+//    private ArrayList<String> getDirectPaths(String node)
+//    {
+//        retru
+//    }
+}
