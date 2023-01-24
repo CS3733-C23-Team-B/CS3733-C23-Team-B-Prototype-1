8192872eb10e969317447669eefc5095845ab0d2
JackLafond <jacklafo12@gmail.com>
2023-01-24 15:07:56 -0500

    making this class dormant

--- src/main/java/edu/wpi/teamname/database/GetOriginalData.java
+++ src/main/java/edu/wpi/teamname/database/GetOriginalData.java
@@ -1,18 +1,36 @@
 package edu.wpi.teamname.database;
-import java.io.FileReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import resources.dataSources.L1Nodes.csv;
+import java.util.Scanner;
+
+
 public class GetOriginalData {
 
-    public ArrayList<Node> getNodesFromCSV() {
+  public ArrayList<Node> getNodesFromCSV() throws FileNotFoundException {
 
-        FileReader fr = new FileReader();
-        ArrayList<Node> csvNodes = new ArrayList<Node>();
 
+    File file = new File("./OriginalData.L1Nodes.csv");
+    Scanner scan = new Scanner(file);
+    scan.useDelimiter(" ");
+    ArrayList<Node> csvNodes = new ArrayList<Node>();
+    while (scan.hasNext()) {
+      String nodeID = scan.next();
+      System.out.println("nodeID");
     }
 
-    public ArrayList<Edge> getEdgesFromCSV() {
+    return csvNodes;
+  }
 
-    }
+  /*public ArrayList<Edge> getEdgesFromCSV() throws FileNotFoundException {
 
+    File file = new File("./OriginalData.L1Edges.csv");
+    FileReader fr = new FileReader(file);
+    ArrayList<Edge> csvEdges = new ArrayList<Edge>();
+
+    return csvEdges;
+
+
+  }*/
 }
