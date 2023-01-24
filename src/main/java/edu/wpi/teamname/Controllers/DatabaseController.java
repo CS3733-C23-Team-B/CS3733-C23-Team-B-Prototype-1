c202dd95ab972bea41da3d8e70ce191810298a68
Sam Colebourn <93502825+ColebournS@users.noreply.github.com>
2023-01-24 18:17:27 -0500

    working node ui query

--- src/main/java/edu/wpi/teamname/Controllers/DatabaseController.java
+++ src/main/java/edu/wpi/teamname/Controllers/DatabaseController.java
@@ -1,19 +1,22 @@
 package edu.wpi.teamname.Controllers;
 
 import edu.wpi.teamname.Database.Node;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DatabaseController {
   @FXML Button nodeSearchButton;
+
   /** Method run when controller is initialized */
   public void initialize() {
     nodeSearchButton.setOnAction((actionEvent) -> getData());
@@ -41,18 +44,48 @@ public class DatabaseController {
     }
     nodeBox.getChildren().clear();
     nodeBox.getChildren().add(new Label("Current Nodes:"));
-    for (Node thing : nodes) {
-      System.out.println("Node Id:" + thing.getID());
-      nodeBox
-          .getChildren()
-          .add(new Label("Node:  " + thing.getID() + "  Floor:  " + thing.getFloor()));
+    for (Node node : nodes) {
+      nodeBox.getChildren().add(new Label(node.getInfo()));
     }
     bor.setCenter(nodeBox);
     Button b = new Button();
     b.setText("Back");
-    
+    b.setOnAction(
+        e -> {
+          Stage stage = (Stage) nodeBox.getScene().getWindow();
+          FXMLLoader loader =
+              new FXMLLoader(getClass().getResource("/edu/wpi/teamname/views/Navigation.fxml"));
+          Parent root = null;
+          try {
+            root = loader.load();
+          } catch (IOException ex) {
+            throw new RuntimeException(ex);
+          }
+          root.setId("pane");
+          Scene scene = new Scene(root, 800, 600);
+          scene
+              .getStylesheets()
+              .addAll(this.getClass().getResource("/css/style.css").toExternalForm());
+          // locking the stage size
+          stage.setMinHeight(600);
+          stage.setMinWidth(800);
+          stage.setMaxHeight(600);
+          stage.setMaxWidth(800);
+
+          stage.setScene(scene);
+          stage.show();
+
+          FXMLLoader homeLoader =
+              new FXMLLoader(getClass().getResource("/edu/wpi/teamname/views/DatabaseUI.fxml"));
+          BorderPane borderPane = (BorderPane) root;
+          try {
+            borderPane.setCenter(homeLoader.load());
+          } catch (IOException ex) {
+            throw new RuntimeException(ex);
+          }
+        });
     bor.setBottom(b);
-    Scene scene = new Scene(bor, 800, 900);
+    Scene scene = new Scene(bor, 1000, 900);
 
     Stage stage = (Stage) nodeSearchButton.getScene().getWindow();
     stage.setScene(scene);
