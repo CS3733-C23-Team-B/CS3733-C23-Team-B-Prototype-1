78f623824e8c260829561e1659fc0e8c3954a5a9
JackLafond <jacklafo12@gmail.com>
2023-01-24 20:08:29 -0500

    Update Edge.java

--- src/main/java/edu/wpi/teamname/Database/Edge.java
+++ src/main/java/edu/wpi/teamname/Database/Edge.java
@@ -68,7 +68,7 @@ public class Edge {
   }
 
   public void update() throws SQLException {
-    String sql = "UPDATE edge SET startNode = ?, endNode = ?,"
+    String sql = "UPDATE edge SET startNode = ?, endNode = ?"
             +" WHERE edgeID = '?';";
     PreparedStatement ps = Bdb.prepareStatement(sql);
     ps.setString(3, edgeID);
