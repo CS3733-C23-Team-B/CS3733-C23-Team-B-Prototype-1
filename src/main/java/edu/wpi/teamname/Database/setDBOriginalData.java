36d55b8b90fb02629ea0bfa15d4263ba1be3e5cc
JackLafond <jacklafo12@gmail.com>
2023-01-24 19:06:50 -0500

    Restore Original Data File finsihed

---
+++ src/main/java/edu/wpi/teamname/Database/setDBOriginalData.java
@@ -0,0 +1,31 @@
+package edu.wpi.teamname.Database;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class setDBOriginalData {
+
+  public void runSQL(File sqlCsv) throws FileNotFoundException, SQLException {
+
+    Scanner scan = new Scanner(sqlCsv);
+    while (scan.hasNext()) {
+      String sql = scan.next();
+      PreparedStatement ps = Bdb.prepareStatement(sql);
+      ps.executeUpdate();
+    }
+    scan.close();
+  }
+
+  public void setData() throws SQLException, FileNotFoundException {
+
+    File nodeFile = new File("./OriginalData.L1NodesSQL.csv");
+    File edgeFile = new File("./OriginalData.L1EdgesSQL.csv");
+    runSQL(nodeFile);
+    runSQL(edgeFile);
+
+  }
+}
