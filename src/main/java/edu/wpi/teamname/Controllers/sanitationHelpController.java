ea43f951b7d063ff43c3c7815d224993f2361a20
lukegrady1 <lukegrady9@outlook.com>
2023-01-25 19:57:34 -0500

    help Controller

--- src/main/java/edu/wpi/teamname/Controllers/sanitationHelpController.java
+++ src/main/java/edu/wpi/teamname/Controllers/sanitationHelpController.java
@@ -1,3 +1,21 @@
 package edu.wpi.teamname.Controllers;
 
-public class sanitationHelpController {}
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class sanitationHelpController {
+
+  @FXML private Button backButton;
+
+  public void backButtonClicked() throws IOException {
+    FXMLLoader loader2 =
+        new FXMLLoader(getClass().getResource("/edu/wpi/teamname/views/sanitationService.fxml"));
+    Parent root2 = loader2.load();
+    BorderPane b = (BorderPane) backButton.getScene().getRoot();
+    b.setCenter(root2);
+  }
+}
