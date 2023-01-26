b0e690289720c15f8566eb72d908f7efbdfa7dda
Cierra <cierra362@gmail.com>
2023-01-25 20:43:22 -0500

    csvWriter handles special characters

--- src/main/java/edu/wpi/teamname/csvWriter.java
+++ src/main/java/edu/wpi/teamname/csvWriter.java
@@ -5,13 +5,29 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class csvWriter {
-  public void csvWriter() {}
-
-  public static void writeCsv(String fileName, String data) throws IOException {
-
+  public static void writeCsv(String fileName, String[] data) throws IOException {
     File csvFile = new File("./src/main/resources/dataSources/" + fileName + ".csv");
     FileWriter fileWriter = new FileWriter(csvFile, true);
-    fileWriter.write(data);
+    fileWriter.write(convertToCSV(data));
     fileWriter.close();
+  }
+
+  private static String convertToCSV(String[] data) {
+    String csv = "";
+
+    for (int i = 0; i < data.length; i++) {
+      String s = data[i];
+
+      // Remove special characters
+      s = s.replaceAll("\\R", " ");
+      if (s.contains(",") || s.contains("\"") || s.contains("'")) {
+        s.replace("\"", "\"\"");
+        s = "\"" + s + "\"";
+      }
+
+      csv = csv + s;
+      if (i != data.length - 1) csv = csv + ",";
+    }
+    return csv;
   }
 }
