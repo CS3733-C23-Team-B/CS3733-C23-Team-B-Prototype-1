01e74ff904bc62d336db24604f3f143ad2582e10
swlendrum <slendrum465@gmail.com>
2023-01-27 16:28:32 -0500

    Login credentials

--- src/main/java/edu/wpi/teamb/Controllers/Controller.java
+++ src/main/java/edu/wpi/teamb/Controllers/Controller.java
@@ -1,26 +1,67 @@
 package edu.wpi.teamb.Controllers;
 
+import edu.wpi.teamb.Database.Login;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Controller {
   @FXML private Button homeButton;
   @FXML private BorderPane border;
   @FXML private Button signInButton;
+  @FXML private TextField username;
+  @FXML private TextField password;
+  @FXML private CheckBox newAccount;
+  @FXML private Label prompt;
 
-  public void handleKeyPress(KeyEvent event) throws IOException {
+  private final String USER = "bodacious";
+  private final String PASS = "badgers";
+
+  private Map<String, Login> users;
+
+  {
+    try {
+      users = Login.getAll();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void handleKeyPress(KeyEvent event) throws IOException, SQLException {
     if (event.getCode().equals(KeyCode.ENTER)) signInButtonClicked();
   }
 
-  public void signInButtonClicked() throws IOException {
+  public boolean validateLogin() throws SQLException {
+    if (username.getText().equals(USER) && password.getText().equals(PASS)) return true;
+    else if (users.containsKey(username.getText())
+        && users.get(username.getText()).getPassword().equals(password.getText())) return true;
+    else if (newAccount.isSelected()) {
+      Login newLogin = new Login(username.getText(), password.getText(), "");
+      newLogin.insert();
+      return true;
+    }
+    prompt.setText("\tInvalid login");
+    prompt.setTextFill(Color.RED);
+    username.clear();
+    password.clear();
+    return false;
+  }
+
+  public void signInButtonClicked() throws IOException, SQLException {
+    if (!validateLogin()) return;
     Stage stage = (Stage) signInButton.getScene().getWindow();
     FXMLLoader loader =
         new FXMLLoader(getClass().getResource("/edu/wpi/teamb/views/Navigation.fxml"));
